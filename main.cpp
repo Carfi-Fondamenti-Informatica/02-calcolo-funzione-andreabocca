1a619103f9a149216192043969b34746aa6fce3c
andreabocca <127606319+andreabocca@users.noreply.github.com>
2023-04-15 12:05:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0,b=0,x=0,y=0,z=0;
+    bool c=0,d=0,e=0;
+    cin>>a>>b>>x>>y;
+
+    c=((x<0)and(y>0));
+    d=((x>=0)and(y<=0));
+    e=((c==0)and(d==0));
+
+    if (c==1){z=(a*x)-(b*y);}
+    if (d==1){z=(a*x*x)-(b*y);}
+    if (e==1){z=(a*x)+(b*y*y);}
+
+    cout<<z<<endl;
+    return 0;
 }
